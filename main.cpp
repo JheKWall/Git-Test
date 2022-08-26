ffe4f2eff0d7b7b1748d6ad3d8c05ef526f6197f
Jhems Wallace <jhekwall@ut.utm.edu>
2022-08-26 11:53:59 -0500

    Second commit: program main.cpp added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Kenneth Wallace
+ * @date 2022-08-26
+ * @brief Prints "Hello World!"
+ * 
+ * Utilizes the console output functionality to print a string of text ("Hello World!") to the console when the program is run.
+ */
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
